f8309e721609cb1eaae88aee29ca236ba9c700bd
Vladimir Toropov <toropov29@rambler.ru>
2020-10-14 02:21:54 +0300

    It's working!

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,16 +6,12 @@ def main():
     StartState = np.array([0, 4, 3, 6, 2, 1, 7, 5, 8])
     Tree = Node(state=StartState)
     Current = Tree
-    k = 0
     while Current.Depth <= 30:
         Current.getstate()
         Current.expand()
         if Current.goaltest():
-            # return 'Pobyeda'
-            pass
+            return 'Pobyeda'
         Current = Current.getchild()[0]
-        print(k)
-        k += 1
 
 
 if __name__ == "__main__":
--- lab_1/tree.py
+++ lab_1/tree.py
@@ -2,21 +2,18 @@ import numpy as np
 
 
 class Node:
-    State = np.array([])
-    Prev = np.NaN
-    Childs = []
+
     GoalState = np.array([1, 2, 3, 4, 0, 5, 6, 7, 8])
-    Action = np.NaN
-    Depth = 0
 
     def __init__(self, state, action=np.NaN, prev=np.NaN, depth=0):
         self.State = state
         self.Prev = prev
         self.Depth = depth
         self.Action = action
+        self.Childs = []
 
     def goaltest(self):
-        if self.State.all() == self.GoalState.all():
+        if np.array_equal(self.State, self.GoalState):
             return True
         else:
             return False
@@ -27,10 +24,10 @@ class Node:
     def getstate(self):
         for i in range(3):
             print(self.State[i*3:i*3 + 3])
-        print("next: ")
+        print("\n")
 
     def actionproof(self, action):
-        return not self.Action - action == np.abs(1)
+        return not np.abs(self.Action - action) == 1
 
     def expand(self):
         IndexZero = np.where(self.State == 0)[0]
@@ -38,19 +35,15 @@ class Node:
             NewState = self.State.copy()
             NewState[IndexZero], NewState[IndexZero - 1] = NewState[IndexZero - 1], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=0, prev=self, depth=self.Depth+1))
-            print(self.State, NewState, 0)
         if IndexZero[0] % 3 < 2 and self.actionproof(1):
             NewState = self.State.copy()
             NewState[IndexZero], NewState[IndexZero + 1] = NewState[IndexZero + 1], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=1, prev=self, depth=self.Depth+1))
-            print(self.State, self.Childs[0].State, NewState, len(self.Childs), 1)
-        if IndexZero[0] // 3 < 2 and self.actionproof(2):
+        if IndexZero[0] // 3 < 2 and self.actionproof(3):
             NewState = self.State.copy()
             NewState[IndexZero], NewState[IndexZero + 3] = NewState[IndexZero + 3], NewState[IndexZero]
-            self.Childs.append(Node(state=NewState, action=2, prev=self, depth=self.Depth+1))
-            print(self.State, NewState, 2)
-        if IndexZero[0] // 3 > 0 and self.actionproof(3):
+            self.Childs.append(Node(state=NewState, action=3, prev=self, depth=self.Depth+1))
+        if IndexZero[0] // 3 > 0 and self.actionproof(4):
             NewState = self.State.copy()
             NewState[IndexZero], NewState[IndexZero - 3] = NewState[IndexZero - 3], NewState[IndexZero]
-            self.Childs.append(Node(state=NewState, action=3, prev=self, depth=self.Depth+1))
-            print(self.State, NewState, 3)
+            self.Childs.append(Node(state=NewState, action=4, prev=self, depth=self.Depth+1))
