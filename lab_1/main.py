fbc53727d934b689c2d8ced54eed030252ae3cc8
Vladimir Toropov <toropov29@rambler.ru>
2020-10-14 02:00:07 +0300

    It's working...almost

--- lab_1/main.py
+++ lab_1/main.py
@@ -6,13 +6,16 @@ def main():
     StartState = np.array([0, 4, 3, 6, 2, 1, 7, 5, 8])
     Tree = Node(state=StartState)
     Current = Tree
+    k = 0
     while Current.Depth <= 30:
+        Current.getstate()
         Current.expand()
         if Current.goaltest():
-            return 'Pobyeda'
-        Current = Current.Childs[0]
-        print()
-        Current.getstate()
+            # return 'Pobyeda'
+            pass
+        Current = Current.getchild()[0]
+        print(k)
+        k += 1
 
 
 if __name__ == "__main__":
--- lab_1/tree.py
+++ lab_1/tree.py
@@ -16,30 +16,41 @@ class Node:
         self.Action = action
 
     def goaltest(self):
-        if self.State == self.GoalState:
+        if self.State.all() == self.GoalState.all():
             return True
         else:
             return False
 
+    def getchild(self):
+        return self.Childs
+
     def getstate(self):
         for i in range(3):
-            print(self.State[i*3:i*3 + 2])
+            print(self.State[i*3:i*3 + 3])
+        print("next: ")
 
     def actionproof(self, action):
         return not self.Action - action == np.abs(1)
 
     def expand(self):
-        IndexZero = np.where(self.State == 0)
+        IndexZero = np.where(self.State == 0)[0]
         if IndexZero[0] % 3 > 0 and self.actionproof(0):
-            NewState = self.State.swap(IndexZero[0], IndexZero - 1)
-            # NewState =
+            NewState = self.State.copy()
+            NewState[IndexZero], NewState[IndexZero - 1] = NewState[IndexZero - 1], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=0, prev=self, depth=self.Depth+1))
+            print(self.State, NewState, 0)
         if IndexZero[0] % 3 < 2 and self.actionproof(1):
-            NewState = self.State.swap(IndexZero[0], IndexZero + 1)
+            NewState = self.State.copy()
+            NewState[IndexZero], NewState[IndexZero + 1] = NewState[IndexZero + 1], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=1, prev=self, depth=self.Depth+1))
+            print(self.State, self.Childs[0].State, NewState, len(self.Childs), 1)
         if IndexZero[0] // 3 < 2 and self.actionproof(2):
-            NewState = self.State.swap(IndexZero[0], IndexZero + 3)
+            NewState = self.State.copy()
+            NewState[IndexZero], NewState[IndexZero + 3] = NewState[IndexZero + 3], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=2, prev=self, depth=self.Depth+1))
+            print(self.State, NewState, 2)
         if IndexZero[0] // 3 > 0 and self.actionproof(3):
-            NewState = self.State.swap(IndexZero[0], IndexZero - 3)
+            NewState = self.State.copy()
+            NewState[IndexZero], NewState[IndexZero - 3] = NewState[IndexZero - 3], NewState[IndexZero]
             self.Childs.append(Node(state=NewState, action=3, prev=self, depth=self.Depth+1))
+            print(self.State, NewState, 3)
